f83e2389e5e70aeb5ff6903ade1290e89c656ccd
suzushin54 <mkm.snicr@gmail.com>
2024-07-14 22:17:25 +0900

    chore: Enhance app to list ECR repositories and allow user to select one to view images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use structopt::StructOpt;
 use aws_config::BehaviorVersion;
 use aws_sdk_ecr::Client;
+use std::io::{self, Write};
 
 #[derive(StructOpt)]
 struct Cli {}
@@ -16,24 +17,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let res = client.describe_repositories().send().await?;
 
     if let Some(repositories) = res.repositories {
-        for repo in repositories {
-            let repo_name = repo.repository_name.unwrap_or_default();
-            println!("Repository: {}", repo_name);
+        println!("ECR Repositories:");
+        for (index, repo) in repositories.iter().enumerate() {
+            println!("{}. {}", index + 1, repo.repository_name.as_deref().unwrap_or("Unknown"));
+        }
 
-            // Listing docker images
-            let images = client
-                .list_images()
-                .repository_name(&repo_name)
-                .send()
-                .await?;
+        // Prompt the user to select a repository
+        print!("Select a repository by number: ");
+        io::stdout().flush()?;
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let selected_index: usize = input.trim().parse()?;
 
-            if let Some(image_ids) = images.image_ids {
-                for image_id in image_ids {
-                    println!(" Image: {:?}", image_id.image_tag.unwrap_or_default());
-                }
+        if selected_index == 0 || selected_index > repositories.len() {
+            println!("Invalid selection");
+            return Ok(());
+        }
+
+        let selected_repo = &repositories[selected_index - 1];
+        let repo_name = selected_repo.repository_name.as_deref().unwrap_or("Unknown");
+
+        // List images in the selected repository
+        println!("Selected repository: {}", repo_name);
+        let images_resp = client
+            .list_images()
+            .repository_name(repo_name)
+            .send()
+            .await?;
+
+        if let Some(image_ids) = images_resp.image_ids {
+            if image_ids.is_empty() {
+                println!("No images found in repository");
             } else {
-                println!(" No images found in repository");
+                println!("Images in repository '{}':", repo_name);
+                for image_id in image_ids {
+                    println!("  Image: {:?}", image_id.image_tag.as_deref().unwrap_or("No tag"));
+                }
             }
+        } else {
+            println!("No images found in repository");
         }
     } else {
         println!("No repositories found");
